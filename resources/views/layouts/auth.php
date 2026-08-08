368a54f30d9a2b7f46729401c779dbb48a15efa1
Sumit Gola <sumitgola30090@gmail.com>
2026-08-08 08:59:36 +0530

    Add shared animated neon backdrop

--- resources/views/layouts/auth.php
+++ resources/views/layouts/auth.php
@@ -13,10 +13,11 @@ $siteName = \App\Services\SeoService::siteName();
     <title><?= e($pageTitle) ?> | <?= e($siteName) ?></title>
     <link rel="icon" href="<?= asset('favicon.png') ?>" type="image/png" sizes="512x512">
     <link rel="apple-touch-icon" href="<?= asset('apple-touch-icon.png') ?>" sizes="180x180">
-    <link rel="stylesheet" href="<?= asset('assets/css/app.css') ?>?v=20260807category1">
+    <link rel="stylesheet" href="<?= asset('assets/css/app.css') ?>?v=20260808neon-field1">
 </head>
 <body class="auth-shell auth-page">
     <?php require base_path('resources/views/partials/google-tag-manager-body.php'); ?>
+    <?php require base_path('resources/views/partials/neon-field.php'); ?>
     <a class="skip-link" href="#main-content">Skip to content</a>
     <?php require base_path('resources/views/partials/site-header.php'); ?>
     <main class="auth-main" id="main-content">
---
+++ resources/views/partials/neon-field.php
@@ -0,0 +1,8 @@
+<div class="site-neon-field" aria-hidden="true">
+    <span class="site-neon-wave site-neon-wave--one"></span>
+    <span class="site-neon-wave site-neon-wave--two"></span>
+    <span class="site-neon-wave site-neon-wave--three"></span>
+    <span class="site-neon-flare site-neon-flare--one"></span>
+    <span class="site-neon-flare site-neon-flare--two"></span>
+    <span class="site-neon-flare site-neon-flare--three"></span>
+</div>
