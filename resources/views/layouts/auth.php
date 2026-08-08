7541248d7e1c5d77558a3e7f80e0cb42bb285167
Sumit Gola <sumitgola30090@gmail.com>
2026-08-08 09:22:08 +0530

    Add animated neon circuit field

--- resources/views/layouts/auth.php
+++ resources/views/layouts/auth.php
@@ -13,7 +13,7 @@ $siteName = \App\Services\SeoService::siteName();
     <title><?= e($pageTitle) ?> | <?= e($siteName) ?></title>
     <link rel="icon" href="<?= asset('favicon.png') ?>" type="image/png" sizes="512x512">
     <link rel="apple-touch-icon" href="<?= asset('apple-touch-icon.png') ?>" sizes="180x180">
-    <link rel="stylesheet" href="<?= asset('assets/css/app.css') ?>?v=20260808neon-field1">
+    <link rel="stylesheet" href="<?= asset('assets/css/app.css') ?>?v=20260808neon-circuit1">
 </head>
 <body class="auth-shell auth-page">
     <?php require base_path('resources/views/partials/google-tag-manager-body.php'); ?>
--- resources/views/partials/neon-field.php
+++ resources/views/partials/neon-field.php
@@ -1,4 +1,43 @@
 <div class="site-neon-field" aria-hidden="true">
+    <svg class="site-neon-circuit" viewBox="0 0 1600 1000" preserveAspectRatio="xMidYMid slice" focusable="false">
+        <defs>
+            <path id="circuit-route-alpha" d="M-60 116H182V190H386V126H610V218H824V164H1030" />
+            <path id="circuit-route-beta" d="M1660 102H1452V178H1264V278H1048V226H902V344H706" />
+            <path id="circuit-route-gamma" d="M-70 468H164V390H346V512H566V438H766V568H1018V486H1214V540H1660" />
+            <path id="circuit-route-delta" d="M-40 862H218V784H410V690H638V770H830V676H1070V748H1282V650H1660" />
+            <path id="circuit-route-epsilon" d="M140 1060V842H316V642H238V556H82" />
+            <path id="circuit-route-zeta" d="M1496-60V152H1390V346H1494V462H1608" />
+        </defs>
+
+        <g class="site-circuit-map">
+            <use class="site-circuit-trace site-circuit-cyan" href="#circuit-route-alpha" />
+            <use class="site-circuit-trace site-circuit-violet" href="#circuit-route-beta" />
+            <use class="site-circuit-trace site-circuit-cyan" href="#circuit-route-gamma" />
+            <use class="site-circuit-trace site-circuit-teal" href="#circuit-route-delta" />
+            <use class="site-circuit-trace site-circuit-violet" href="#circuit-route-epsilon" />
+            <use class="site-circuit-trace site-circuit-teal" href="#circuit-route-zeta" />
+        </g>
+
+        <g class="site-circuit-energy">
+            <use class="site-circuit-flow site-circuit-cyan" href="#circuit-route-alpha" />
+            <use class="site-circuit-flow site-circuit-violet site-circuit-flow--reverse" href="#circuit-route-beta" />
+            <use class="site-circuit-flow site-circuit-cyan site-circuit-flow--slow" href="#circuit-route-gamma" />
+            <use class="site-circuit-flow site-circuit-teal site-circuit-flow--reverse" href="#circuit-route-delta" />
+            <use class="site-circuit-flow site-circuit-violet site-circuit-flow--short" href="#circuit-route-epsilon" />
+            <use class="site-circuit-flow site-circuit-teal site-circuit-flow--short site-circuit-flow--reverse" href="#circuit-route-zeta" />
+        </g>
+
+        <g class="site-circuit-nodes">
+            <circle class="site-circuit-node site-circuit-cyan" cx="386" cy="126" r="6" />
+            <circle class="site-circuit-node site-circuit-violet" cx="1264" cy="278" r="6" />
+            <circle class="site-circuit-node site-circuit-cyan" cx="566" cy="438" r="6" />
+            <circle class="site-circuit-node site-circuit-cyan" cx="1214" cy="540" r="6" />
+            <circle class="site-circuit-node site-circuit-teal" cx="830" cy="676" r="6" />
+            <circle class="site-circuit-node site-circuit-teal" cx="1282" cy="650" r="6" />
+            <circle class="site-circuit-node site-circuit-violet" cx="316" cy="642" r="6" />
+            <circle class="site-circuit-node site-circuit-teal" cx="1390" cy="346" r="6" />
+        </g>
+    </svg>
     <span class="site-neon-wave site-neon-wave--one"></span>
     <span class="site-neon-wave site-neon-wave--two"></span>
     <span class="site-neon-wave site-neon-wave--three"></span>
